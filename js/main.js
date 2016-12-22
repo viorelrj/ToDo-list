a6bf184ca41bfa688dfd6bb129c804891fef13f1
viorelrj <viorelrinja@gmail.com>
2016-12-22 22:59:47 +0200

    Sortable
    Now List Items Are sortable. Rewote the saving logic.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
+	function ItemObj (content, state) {
+		this.content = content;
+		this.check = state;
+	}
 	if (localStorage.getItem('data') != null) {
 		var data = JSON.parse(localStorage['data']);
-		console.log(data);
-		for (var i = 0; i < data.length; i++) {
-			$('.list__items').prepend('<div class="panel panel-default list__item"> <div class="panel-body"> <div class="col-xs-12 col-sm-10"> <p class="list__content">' + data[i] + '</p> </div> <div class="btn-group item__opts js-item__opts" role="group" aria-label="..."> <button type="button" class="btn btn-default js-btn--check btn--check"> <i class="fa fa-check" aria-hidden="true"></i> </button> <button type="button" class="btn btn-default js-btn--delete btn--delete"> <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div> </div> </div>');
+		for (var i = data.length - 1; i >= 0 ; i--) {
+			var checked = (data[i].check) ? 'list__item--check' : null;
+			$('.list__items').prepend('<div class="panel panel-default list__item '+ checked +'"> <div class="panel-body"> <div class="col-xs-12 col-sm-10"> <p class="list__content">' + data[i].content + '</p> </div> <div class="btn-group item__opts js-item__opts" role="group" aria-label="..."> <button type="button" class="btn btn-default js-btn--check btn--check"> <i class="fa fa-check" aria-hidden="true"></i> </button> <button type="button" class="btn btn-default js-btn--delete btn--delete"> <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div> </div> </div>');
 		} 
-	} else {
-		var data = new Array();
 	}
 
+	$('.list__items').sortable();
+
+	function saveState () {
+		var data = new Array();
+		$('.list__item').each(function(){
+			var itemData = new ItemObj();
+			itemData.content = $(this).find('.list__content').text();
+			itemData.check = ($(this).hasClass('list__item--check')) ? true : false;
+			data.push(itemData);
+			itemData = {};	
+			JSONdata = JSON.stringify(data);	
+			localStorage.setItem('data', JSONdata);	
+		});		
+	}
 	//preventing the enter button submitting the form and reloading
 	$(document).on('submit', '#input', function(e){
 		e.preventDefault();
 	});
+
+	//Making the Items sortable
 
 	//ading new Item
 	$(document).on('click', '.js-btn--add', function(){
@@ -24,32 +42,28 @@ $(document).ready(function(){
 
 		var $input = $('input[name = list-input]').val();
 			if ($input) {
-				$('.list__items').prepend('<div class="panel panel-default list__item"> <div class="panel-body"> <div class="col-xs-12 col-sm-10"> <p class="list__content">' + $input + '</p> </div> <div class="btn-group item__opts js-item__opts" role="group" aria-label="..."> <button type="button" class="btn btn-default js-btn--check btn--check"> <i class="fa fa-check" aria-hidden="true"></i> </button> <button type="button" class="btn btn-default js-btn--delete btn--delete"> <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div> </div> </div>');
-				data.push($input);	
-				JSONdata = JSON.stringify(data);	
-				localStorage.setItem('data', JSONdata);		
+				$('.list__items').prepend('<div class="panel panel-default list__item"> <div class="panel-body"> <div class="col-xs-12 col-sm-10"> <p class="list__content">' + $input + '</p> </div> <div class="btn-group item__opts js-item__opts" role="group" aria-label="..."> <button type="button" class="btn btn-default js-btn--check btn--check"> <i class="fa fa-check" aria-hidden="true"></i> </button> <button type="button" class="btn btn-default js-btn--delete btn--delete"> <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div> </div> </div>').sortable();	
+				//item.content = $input;
 			}
-
+			saveState();
 		var $input = $('input[name = list-input]').val('');
 	});
 
 	//Checking the Item
 	$(document).on('click', '.js-btn--check', function(){
-		$(this).closest('.panel-body').find('.list__content').toggleClass('list__content--check');
+		$(this).closest('.list__item').toggleClass('list__item--check');
+		saveState();
 	});
 
 	//Deleting the Item
 	$(document).on('click', '.js-btn--delete', function(){
 		$(this).closest('.list__item').toggleClass('list__item--delete').remove();
-		var del = $(this).closest('.list__item').find('.list__content').text();
-		var index = data.indexOf(del);
-		data.splice(index, 1);
-		JSONdata = JSON.stringify(data);	
-		localStorage.setItem('data', JSONdata);	
+		saveState();
 	});
 
 	//Displaying the Item menu
 	$(document).on('mouseenter mouseleave', '.list__item', function(){
 		$(this).find('.item__opts').toggleClass('item__opts--hover');
+		saveState();
 	});
 });
